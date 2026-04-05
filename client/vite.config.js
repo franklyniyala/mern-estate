c63889d44a3e1a1582ec36090e16b3d54e8b4a16
franklyn <franklyniyala@gmail.com>
2026-04-05 20:38:26 +0100

    Initial commit

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,10 +3,13 @@ import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  plugins: [react()],
   server: {
+    host: '0.0.0.0',
+    ports: 5173,
     proxy: {
       '/api': {
-        target: 'http://localhost:3000',
+        target: 'http://api:3000',
         secure: false,
       },
     },
